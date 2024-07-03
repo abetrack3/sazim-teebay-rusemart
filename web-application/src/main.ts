a30c73470cce3a3b9b542a8da4ca221bbb011712
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-04 03:36:38 +0600

    moved server initialization, cleaner main.ts

--- web-application/src/main.ts
+++ web-application/src/main.ts
@@ -1,16 +1,3 @@
-import express, { Application, Request, Response } from 'express';
-import exampleRoutes from './routes/exampleRoutes';
+import { initServer } from "./servers";
 
-const app: Application = express();
-const port: number = 3000;
-
-app.use(express.json());
-app.use('/api', exampleRoutes);
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+initServer();
---
+++ web-application/src/servers/index.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from 'express';
+import exampleRoutes from '../routes/exampleRoutes';
+
+export const initServer = () => {
+
+    const app: Application = express();
+    const port: number = 3000;
+
+    app.use(express.json());
+    app.use('/api', exampleRoutes);
+
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Hello World!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+
+}
