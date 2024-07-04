5f61ca72d82edef06b43a634f96935e28e1f1de9
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-04 14:04:46 +0600

    environment variable type inference support added

--- web-client/src/vite-env.d.ts
+++ web-client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APOLLO_GRAPHQL_API_ENDPOINT: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
