acd61ed9dfafe02b0e5199cc2fdea9835aac3d61
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-06 14:04:35 +0600

    configuring apolloclient to send token header with every request once user is logged in

--- web-client/src/graphql/client.ts
+++ web-client/src/graphql/client.ts
@@ -1,9 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const apiUrl: string = import.meta.env.VITE_APOLLO_GRAPHQL_API_ENDPOINT;
 
-const apolloClient = new ApolloClient({
+const httpLink = createHttpLink({
 	uri: apiUrl,
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('token');
+	return {
+		headers: {
+			...headers,
+			token: token ?? "",
+		}
+	}
+});
+
+const apolloClient = new ApolloClient({
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
 
