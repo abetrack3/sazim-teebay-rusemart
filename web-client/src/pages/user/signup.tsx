bfbd70c9cd5431ec4951ee0cebcf0fb5cc8f30fd
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-06 11:22:08 +0600

    restoring signup page to its original implementation

--- web-client/src/pages/user/signup.tsx
+++ web-client/src/pages/user/signup.tsx
@@ -1,10 +1,12 @@
-import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
-import SignupForm from "../../components/forms/user-signup";
-
-
+import { Box, Button, TextField } from "@mui/material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Link, useNavigate } from "react-router-dom";
+import signupValidationSchema from "../../validators/signup.form.validation.schema";
+import { createUser } from "../../services/user.service";
 
 const SignupPage = () => {
+
+    const navigate = useNavigate();
 
     return (
         <div className="flex flex-col h-full justify-center items-center">
@@ -17,8 +19,49 @@ const SignupPage = () => {
                 px={8}
                 py={6}
             >
+                <Formik
+                    initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        address: '',
+                        email: '',
+                        password: '',
+                        retypePassword: '',
+                    }}
+                    validationSchema={signupValidationSchema}
+                    onSubmit={async (values) => {
+                        console.log(values);
+                        await createUser(values.email, values.firstName, values.lastName, values.address, values.password);
+                        navigate('/login');
+                    }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form className="flex flex-col gap-4">
 
-                <SignupForm></SignupForm>
+                            <Field name="firstName" as={TextField} label="First Name" fullWidth />
+                            <ErrorMessage name="firstName" component="div" />
+
+                            <Field name="lastName" as={TextField} label="Last Name" fullWidth />
+                            <ErrorMessage name="lastName" component="div" />
+
+                            <Field name="address" as={TextField} label="Address" fullWidth />
+                            <ErrorMessage name="address" component="div" />
+
+                            <Field name="email" as={TextField} label="Email" fullWidth />
+                            <ErrorMessage name="email" component="div" />
+
+                            <Field name="password" as={TextField} label="Password" type="password" fullWidth />
+                            <ErrorMessage name="password" component="div" />
+
+                            <Field name="retypePassword" as={TextField} label="Retype Password" type="password" fullWidth />
+                            <ErrorMessage name="retypePassword" component="div" />
+
+                            <Button type="submit" disabled={isSubmitting} variant="contained" color="primary">
+                                Sign Up
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
 
             </Box>
 
@@ -31,4 +74,5 @@ const SignupPage = () => {
 };
 
 export default SignupPage;
+
 
