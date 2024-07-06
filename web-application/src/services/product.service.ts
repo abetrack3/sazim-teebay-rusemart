4f061f5f3c315cc6fe62ce4cfdc46fe6d14e6751
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-06 21:20:43 +0600

    minor better coding practice: semicolon after each statement

--- web-application/src/services/product.service.ts
+++ web-application/src/services/product.service.ts
@@ -3,4 +3,4 @@ import prisma from "../utils/prisma.client";
 
 export const createProduct = async (product: Product) => await prisma.product.create({ data: { ...product }});
 export const getAllProductsByOwnerId = async (ownerId: number) => await prisma.product.findMany({ where: { ownerId }});
-export const deleteProductById = async (id: number, ownerId: number) => await prisma.product.delete({where: { id, ownerId }})
+export const deleteProductById = async (id: number, ownerId: number) => await prisma.product.delete({where: { id, ownerId }});
