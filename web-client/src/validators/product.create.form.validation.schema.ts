fc3a3fbce38e026b52b89872b6b513366ef39acc
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-08 01:26:48 +0600

    validation schema for product create form

---
+++ web-client/src/validators/product.create.form.validation.schema.ts
@@ -0,0 +1,34 @@
+import * as yup from "yup";
+
+const productCreateValidationSchema = yup.object({
+
+    title: yup
+        .string()
+        .required('Product title is required'),
+
+    categories: yup
+        .array().of(yup.string())
+        .min(1, 'At least one categories needs to be selected')
+        .required(),
+
+    description: yup
+        .string()
+        .optional(),
+
+    purchasePrice: yup
+        .number()
+        .moreThan(0)
+        .required('Purchase price is required'),
+
+    rentPrice: yup
+        .number()
+        .moreThan(0)
+        .required('Rent price is required'),
+
+    rentOption: yup
+        .string()
+        .required('Rent option is required'),
+
+});
+
+export default productCreateValidationSchema;
