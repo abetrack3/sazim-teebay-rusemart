29e2b6b3903e6df28d8674a883764110dfd0ea97
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-08 01:46:44 +0600

    material ui autocomplete multi select implementation

---
+++ web-client/src/components/inputs/multi-select-autocomplete.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Autocomplete, TextField } from "@mui/material"
+import { FieldProps } from "formik";
+
+interface MultiSelectDropdownProps {
+    dropDownOptions: string[];
+    label: string;
+    placeHolder: string;
+    [key: string]: string | number | string[] | any;
+}
+
+const MultiSelectDropdownWithAutocomplete = (fieldProps: FieldProps<string> & MultiSelectDropdownProps) => {
+    return (
+        <Autocomplete
+            multiple
+            options={fieldProps.dropDownOptions}
+            getOptionLabel={(option) => option as string}
+            filterSelectedOptions
+            value={fieldProps.form.values[fieldProps.field.name] ?? []}
+            onChange={(_, value) => fieldProps.form.setFieldValue(fieldProps.field.name, value)}
+            renderInput={(params) => (
+            <TextField
+                {...params}
+                label={fieldProps.label}
+                placeholder={fieldProps.placeHolder}
+            />
+            )}
+        />
+    );
+};
+
+export default MultiSelectDropdownWithAutocomplete;
