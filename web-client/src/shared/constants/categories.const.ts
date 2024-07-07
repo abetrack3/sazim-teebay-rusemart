e2d1742ff6574da1b220bef731bdb27fd84bc3a5
Abrar Shahriar Abeed <abrarshahriar2361@gmail.com>
2024-07-08 01:27:55 +0600

    hard coded categories as enum

---
+++ web-client/src/shared/constants/categories.const.ts
@@ -0,0 +1,10 @@
+export enum Categories {
+    ELECTRONICS = 'Electronics',
+    FURNITURE = 'Furniture',
+    HOME_APPLIANCES = 'Home Appliances',
+    SPORTING_GOODS = 'Sporting Goods',
+    OUTDOOR = 'Outdoor',
+    TOYS = 'Toys',
+}
+
+export const CategoryList: string[] = Object.values(Categories);
