a44f588a9588cfdfd83be2c220c799058a2e99dd
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-08 05:53:55 +0600

    all market product query data api integration

--- web-client/src/graphql/queries/product.queries.ts
+++ web-client/src/graphql/queries/product.queries.ts
@@ -18,3 +18,22 @@ query GetAllUserProducts {
 }
 
 `;
+
+export const GET_ALL_MARKET_AVAILABLE_PRODUCTS = gql`
+
+query GetAllMarketAvailableProducts {
+  getAllMarketAvailableProducts {
+    id
+    createdAt
+    updatedAt
+    title
+    categories
+    description
+    purchasePrice
+    rentPrice
+    rentOption
+    ownerId
+  }
+}
+
+`;
--- web-client/src/services/product.service.ts
+++ web-client/src/services/product.service.ts
@@ -1,6 +1,6 @@
 import apolloClient from "../graphql/client"
 import { CREATE_PRODUCT, DELETE_PRODUCT_BY_ID } from "../graphql/mutations/product.mutation";
-import { GET_ALL_USER_PRODUCTS } from "../graphql/queries/product.queries"
+import { GET_ALL_MARKET_AVAILABLE_PRODUCTS, GET_ALL_USER_PRODUCTS } from "../graphql/queries/product.queries"
 import { Product } from "../shared/types/product.types";
 
 export const getAllUserProducts = async () => {
@@ -30,3 +30,13 @@ export const createProduct = async (product: Product) => {
     return data.createProduct as Product;
 
 };
+
+export const getAllMarketAvailableProducts = async () => {
+
+    const { data } = await apolloClient.query({
+        query: GET_ALL_MARKET_AVAILABLE_PRODUCTS,
+    });
+
+    return data.getAllMarketAvailableProducts as Product[];
+
+}
