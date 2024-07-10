5304a74496df6283073d7bbd8db4b15635b3eeb4
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-10 15:24:52 +0600

    created product list component

---
+++ web-client/src/components/product.list.tsx
@@ -0,0 +1,20 @@
+import { Product } from "../shared/types/product.types";
+import ProductCard from "./product.card";
+
+export const ProductList: React.FC<{products: Product[], actionsEnabled: boolean, callbackForDelete?: (id: number) => void}> = ({ products, actionsEnabled, callbackForDelete }) => {
+
+    return (
+        <>
+            {products.map((product, index) => (
+                <ProductCard
+                    key={index}
+                    product={product}
+                    actionsEnabled={actionsEnabled}
+                    callbackForDelete={async () => callbackForDelete ? await callbackForDelete(product.id) : {}}
+                />
+            ))}
+        </>
+    );
+
+}
+
