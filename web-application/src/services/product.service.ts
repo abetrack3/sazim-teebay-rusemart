681186226cdc9f7aabb2dac52d470b0e28acffe9
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-12 17:41:03 +0600

    db integration for 1) get user's product by product id 2) update a product

--- web-application/src/services/product.service.ts
+++ web-application/src/services/product.service.ts
@@ -3,5 +3,7 @@ import prisma from "../utils/prisma.client";
 
 export const createProduct = async (ownerId: string, product: Product) => await prisma.product.create({ data: { ...product, ownerId }});
 export const getAllProductsByOwnerId = async (ownerId: string) => await prisma.product.findMany({ where: { ownerId }});
+export const getUserProductByIdAndOwnerId = async (id: string, ownerId: string) => await prisma.product.findFirst({ where: { id, ownerId }});
 export const deleteProductById = async (id: string, ownerId: string) => await prisma.product.delete({where: { id, ownerId }});
 export const getAllMarketAvailableProducts = async () => await prisma.product.findMany();
+export const updateProductByIdandOwnerId = async (id: string, product: Product, ownerId: string) => await prisma.product.update({ where: { id, ownerId }, data: { ...product }});
