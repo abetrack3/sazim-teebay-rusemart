862178bb562462a8066c5135bf3af5f5f1058bda
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-13 05:43:04 +0600

    my-products is not going to show purchased products, but only the ones the user created himself

--- web-application/src/services/product.service.ts
+++ web-application/src/services/product.service.ts
@@ -2,7 +2,7 @@ import { Product } from "@prisma/client";
 import prisma from "../utils/prisma.client";
 
 export const createProduct = async (ownerId: string, product: Product) => await prisma.product.create({ data: { ...product, ownerId }});
-export const getAllProductsByOwnerId = async (ownerId: string) => await prisma.product.findMany({ where: { ownerId }});
+export const getAllProductsByOwnerId = async (ownerId: string) => await prisma.product.findMany({ where: { ownerId, isSold: false }});
 export const getUserProductByIdAndOwnerId = async (id: string, ownerId: string) => await prisma.product.findFirst({ where: { id, ownerId }});
 export const deleteProductById = async (id: string, ownerId: string) => await prisma.product.delete({where: { id, ownerId }});
 export const getAllMarketAvailableProducts = async () => await prisma.product.findMany();
