0f6fa0ad01e92d6b757ef785dcfbd76e0a8cf5a7
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-13 04:55:11 +0600

    data type for parameters needed to make a product purchase transaction

--- web-application/src/common/model/index.ts
+++ web-application/src/common/model/index.ts
@@ -2,3 +2,12 @@ import { User } from '@prisma/client';
 export interface ApplicationContext {
     authenticatedUser?: User;
 }
+
+export interface ProductPurchaseParameter {
+    sellerId: string;
+    buyerId: string;
+    productId: string;
+    purchasePriceAtTimeOfTransaction: number;
+    rentPriceAtTimeOfTransaction: number;
+    rentOptionAtTimeOfTransaction: string
+}
