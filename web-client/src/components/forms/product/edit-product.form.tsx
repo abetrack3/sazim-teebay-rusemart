8a0a2530382dfcad1ac7124b0e2663bba8489a8f
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-12 18:24:12 +0600

    feature implementation: edit product

---
+++ web-client/src/components/forms/product/edit-product.form.tsx
@@ -0,0 +1,79 @@
+import { Formik } from "formik"
+import { Product } from "../../../shared/types/product.types"
+import productFormValidationSchema from "../../../validators/product.form.validation.schema"
+import { Form, useNavigate } from "react-router-dom"
+import CategoriesInputField from "./fields/categories-input.field"
+import { DescriptionInputField } from "./fields/description-input.field"
+import { PriceInputField } from "./fields/price-input.field"
+import { RentOptionInputField } from "./fields/rent-option-input.field"
+import ProductTitleInput from "./fields/title-input.field"
+import { Button } from "@mui/material"
+import { updateProduct } from "../../../services/product.service"
+
+type EditProductFormProps = {
+    product: Product;
+};
+
+export const EditProductForm: React.FC<EditProductFormProps> = ({ product }) => {
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="w-3/5">
+
+            <Formik
+                initialValues={{
+                    id: product.id,
+                    title: product.title,
+                    categories: product.categories,
+                    description: product.description,
+                    purchasePrice: product.purchasePrice,
+                    rentPrice: product.rentPrice,
+                    rentOption: product.rentOption,
+                }}
+                validationSchema={productFormValidationSchema}
+                onSubmit={values => {
+                    console.log(values);
+                    updateProduct(values as Product);
+                }}
+            >
+
+                {({ isValid, isSubmitting, dirty, submitForm, setSubmitting }) => (
+                    <Form className="flex flex-col gap-4">
+
+                        <ProductTitleInput />
+                        <CategoriesInputField />
+                        <DescriptionInputField />
+                        <div className="flex gap-4 justify-between">
+                            <PriceInputField name="purchasePrice" label="Purchase Price" placeHolder="Enter your Price" />
+                            <PriceInputField name="rentPrice" label="Rent Price" placeHolder="Enter your Rent Price" />
+                            <RentOptionInputField />
+                        </div>
+
+                        <div className="flex justify-between">
+                            <Button variant="outlined" color="primary" onClick={() => navigate('/my-product')}>
+                                Cancel
+                            </Button>
+
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                disabled={!isValid || isSubmitting || !dirty}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    submitForm().finally(() => setSubmitting(false));
+                                }}
+                            >
+                                Submit
+                            </Button>
+                        </div>
+                    </Form>
+
+                )}
+
+            </Formik>
+        </div>
+    )
+
+}
--- web-client/src/pages/my-product/edit.tsx
+++ web-client/src/pages/my-product/edit.tsx
@@ -1,14 +1,68 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Product } from "../../shared/types/product.types";
+import { getUserProductById } from "../../services/product.service";
+import { CircularProgress } from "@mui/material";
+import { EditProductForm } from "../../components/forms/product/edit-product.form";
 
 const EditProductPage = () => {
 
+    const [product, setProduct] = useState<Product>();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
     const { productId } = useParams();
 
-    console.log(productId);
+    useEffect(() => {
+
+        if (!productId) {
+            setError('Something went wrong');
+            return;
+        }
+
+        const fetchProduct = async () => {
+            try {
+                const product = await getUserProductById(productId as string);
+                setProduct(product);
+            } catch (error) {
+                setError('Failed to fetch product');
+            } finally {
+                setLoading(false);
+            }
+
+        };
+
+        fetchProduct();
+
+    }, [productId]);
+
+    if (loading) {
+        return (
+            <div className="flex flex-col h-full justify-center items-center">
+                <CircularProgress />
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="flex flex-col h-full justify-center items-center">
+                <p>{error}</p>
+            </div>
+        );
+    }
 
     return (
         <>
-            <h1>{productId}</h1>
+            <div className="flex flex-col h-full justify-center items-center">
+                {product && <>
+                    <h1 className="text-3xl mb-8">
+                        Edit: {product?.title}
+                    </h1>
+                    <EditProductForm product={product}/>
+                </>}
+                {!product && <h1>Failed to fetch data</h1>}
+            </div>
         </>
     );
 
