bd13d7de9d7054c491508203fd64560a044bdadd
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-13 17:47:20 +0600

    components to display purchased products and sold products

---
+++ web-client/src/pages/my-activity/products.tab.tsx
@@ -0,0 +1,35 @@
+import { ProductList } from "../../components/product.list";
+import { Product } from "../../shared/types/product.types";
+
+const ProductsTab: React.FC<{ products: Product[], productListEmptyText: string }> = ({ products, productListEmptyText }) => {
+
+    return (
+        <div className="w-full flex flex-col justify-center items-center pt-12">
+            {(products.length !== 0) &&
+                <div className='flex flex-col items-center gap-4 w-3/5'>
+                    <ProductList
+                        products={products}
+                        actionsEnabled={false}
+                    />
+                </div>
+            }
+            {products.length === 0 && <p>{productListEmptyText}</p>}
+        </div>
+    );
+};
+
+export const PurchasedProductsTab: React.FC<{ products: Product[] }> = ({ products }) => {
+
+    return <ProductsTab
+        products={products}
+        productListEmptyText="You haven't purchased any product yet. Purchase one?"
+    />
+};
+
+export const SoldProductsTab: React.FC<{ products: Product[] }> = ({ products }) => {
+
+    return <ProductsTab
+        products={products}
+        productListEmptyText="You aven't sold any product yet"
+    />
+};
