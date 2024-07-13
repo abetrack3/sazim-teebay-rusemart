d235b23072942ceb86eb4e9ca06f3e1a95c4a03c
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-13 12:05:40 +0600

    added: layout component for more understandable user navigation

---
+++ web-client/src/components/layout/app-bar.layout.tsx
@@ -0,0 +1,47 @@
+import { MenuTwoTone } from "@mui/icons-material";
+import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
+import { useAuthState } from "../../hooks/useAuthState.hook";
+import { useNavigate } from "react-router-dom";
+
+export const TopNavBar = () => {
+
+    const { isAuthenticated, logout } = useAuthState();
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <AppBar position="sticky" color="transparent">
+                <Toolbar>
+                    <div className="w-full grid grid-cols-5">
+                        <div className="flex justify-center gap-4 col-start-2 col-span-3">
+                            <Button sx={{ color: 'primary', display: 'block'}} onClick={() => navigate('/')}>
+                                Marketplace
+                            </Button>
+                            {isAuthenticated && <>
+                                <Button sx={{ color: 'primary', display: 'block'}} onClick={() => navigate('/my-product')}>
+                                    My Products
+                                </Button>
+                                {/* <Button sx={{ color: 'primary', display: 'block'}}>
+                                    My Activity
+                                </Button> */}
+                            </>}
+                        </div>
+
+                        <div className="col-start-5 flex justify-end">
+                            {isAuthenticated === true &&
+                            <Button type='button' variant='contained' color="error" onClick={logout}>
+                                Logout
+                            </Button>}
+
+                            {isAuthenticated === false &&
+                            <Button type='button' variant='contained' color="primary" onClick={() => navigate('/login')}>
+                                Sign In
+                            </Button>}
+                        </div>
+                    </div>
+                </Toolbar>
+            </AppBar>
+
+        </>
+    );
+};
