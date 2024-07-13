b690ef1d2d5ec0a3137fc8eb43f4b660d3d4a70f
Abrar Shahriar Abeed <abetrack3@yahoo.com>
2024-07-13 12:16:41 +0600

    small ui fine tuning: gap between create product header and form stepper reduced

--- web-client/src/pages/my-product/create.tsx
+++ web-client/src/pages/my-product/create.tsx
@@ -6,7 +6,7 @@ const CreateProductPage = () => {
         <>
             <div className="flex flex-col justify-start items-center pt-12">
                 <div className="w-3/5">
-                    <h1 className="text-3xl mb-20 text-center">CREATE PRODUCT</h1>
+                    <h1 className="text-3xl mb-16 text-center">CREATE PRODUCT</h1>
                 </div>
                 <CreateProductForm />
             </div>
